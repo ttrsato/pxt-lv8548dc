289806541dc56d67fce3d098d411e0af368f0cb9
ttrsato <tatsurosatoh@gmail.com>
2019-05-10 20:08:46 +0900

    Initial release

--- main.ts
+++ main.ts
@@ -1,7 +1,121 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+enum Motor {
+    CH0 = 0,
+    CH1 = 1
+}
+
+enum Rotor_Direction {
+    //% block="Forward-Open"
+    Forward_Open = 0,
+    //% block="Forward-Break"
+    Forward_Break = 1,
+    //% block="Reverse-Open"
+    Reverse_Open = 2,
+    //% block="Reverse-Break"
+    Reverse_Break = 3,
+    //% block="Open"
+    Open = 4,
+    //% block="Break"
+    Break = 5
+}
+
+enum PWM_Freq {
+    //% block="7.813kHz"
+    DIV1_8 = 0,
+    //% block="0.977kHz"
+    DIV1_64 = 1,
+    //% block="0.244kHz"
+    DIV1_256 = 2,
+    //% block="0.061kHz"
+    DIV1_1024 = 3
+}
+
+enum ON_OFF_Flag {
+    //% block="Stop"
+    OFF = 0,
+    //% block="Run"
+    ON = 1
+}
+
+//% weight=70 icon="\uf2db" color=#555555 block="LV8548DC"
+namespace lv8548dc {
+    //% blockId=show_strings block="Init serial tx = %tx rx = %rx"
+    //% tx.defl=SerialPin.P2
+    //% rx.defl=SerialPin.P8
+    export function init(tx: SerialPin, rx: SerialPin): void {
+        serial.redirect(
+            tx,
+            rx,
+            BaudRate.BaudRate19200
+        )
+        basic.showIcon(IconNames.Sad)
+        let init_done = 0
+        while (!(init_done)) {
+            basic.pause(200)
+            let str = serial.readString()
+            if (str[0] == "R") {
+                serial.writeString("A")
+                init_done = 1
+                basic.showIcon(IconNames.Happy)
+                basic.pause(200)
+            }
+        }
+        let bufrini = pins.createBuffer(3)
+        bufrini.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
+        bufrini.setNumber(NumberFormat.UInt8LE, 1, 0xFE)
+        // DC
+        bufrini.setNumber(NumberFormat.UInt8LE, 2, 0x00)
+        serial.writeBuffer(bufrini)
+    }
+
+    //% blockId=lv8548dc_setrotation block="Set %ch motor to %sel"
+    export function setRotation(ch: Motor, sel: Rotor_Direction): void {
+        let bufr = pins.createBuffer(6);
+        // setRotation
+        bufr.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
+        bufr.setNumber(NumberFormat.UInt8LE, 1, 0xFF)
+        bufr.setNumber(NumberFormat.UInt8LE, 2, 0x03)
+        bufr.setNumber(NumberFormat.UInt8LE, 3, 0x44)
+        bufr.setNumber(NumberFormat.UInt8LE, 4, ch)
+        bufr.setNumber(NumberFormat.UInt8LE, 5, sel)
+        serial.writeBuffer(bufr)
+    }
+
+    //% blockId=lv8548dc_setctlvoltage block="Set %ch motor pwm duty = %duty"
+    //% duty.min=0 duty.max=100
+    export function setCtlVoltage(ch: Motor, duty: number): void {
+        let bufr = pins.createBuffer(6);
+        // setRotation
+        bufr.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
+        bufr.setNumber(NumberFormat.UInt8LE, 1, 0xFF)
+        bufr.setNumber(NumberFormat.UInt8LE, 2, 0x03)
+        bufr.setNumber(NumberFormat.UInt8LE, 3, 0x41)
+        bufr.setNumber(NumberFormat.UInt8LE, 4, ch)
+        bufr.setNumber(NumberFormat.UInt8LE, 5, duty)
+        serial.writeBuffer(bufr)
+    }
+
+    //% blockId=lv8548dc_setpwmfreqency block="Set PWM frequency = %freq"
+    export function setPWMFreqency(freq: PWM_Freq): void {
+        let bufr = pins.createBuffer(5);
+        // setRotation
+        bufr.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
+        bufr.setNumber(NumberFormat.UInt8LE, 1, 0xFF)
+        bufr.setNumber(NumberFormat.UInt8LE, 2, 0x02)
+        bufr.setNumber(NumberFormat.UInt8LE, 3, 0x67)
+        bufr.setNumber(NumberFormat.UInt8LE, 4, freq)
+        serial.writeBuffer(bufr)
+    }
+
+    //% blockId=lv8548dc_setstartflag block="%en %ch motor"
+    export function setStartFlag(en: ON_OFF_Flag, ch: Motor): void {
+        let bufr = pins.createBuffer(6);
+        // setRotation
+        bufr.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
+        bufr.setNumber(NumberFormat.UInt8LE, 1, 0xFF)
+        bufr.setNumber(NumberFormat.UInt8LE, 2, 0x03)
+        bufr.setNumber(NumberFormat.UInt8LE, 3, 0x68)
+        bufr.setNumber(NumberFormat.UInt8LE, 4, ch)
+        bufr.setNumber(NumberFormat.UInt8LE, 5, en)
+        serial.writeBuffer(bufr)
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
 // tests go here; this will not be compiled when this package is used as a library
+basic.forever(() => {
+    lv8548dc.init(SerialPin.P0, SerialPin.P1)
+    lv8548dc.setRotation(Motor.CH0, Rotor_Direction.Forward_Open)
+    lv8548dc.setCtlVoltage(Motor.CH0, 200)
+    lv8548dc.setPWMFreqency(PWM_Freq.DIV1_8)
+    lv8548dc.setStartFlag(ON_OFF_Flag.OFF, Motor.CH0)
+})
